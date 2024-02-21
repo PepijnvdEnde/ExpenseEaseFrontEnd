a9a786e1057d3576c58792a671e614bef32f869b
Pepijn  van den Ende <PS.vandenEnde@student.han.nl>
2024-02-21 22:19:33 +0100

    added login function

--- src/components/Loginpage.jsx
+++ src/components/Loginpage.jsx
@@ -9,31 +9,38 @@ class LoginPage extends Component {
         };
     }
 
+
     getLogin = async (username, password) => {
         let data = {
             username: username,
             password: password,
         };
-        console.log(JSON.stringify(data));
-        await fetch("http://localhost:8080/Login", {
+        const response = await fetch("http://localhost:8080/Login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
         });
+
+        return response;
     };
 
     handleLogin = async () => {
-        const response = await this.getLogin(this.state.username, this.state.password).catch((error) => {
-            console.error("Error:", error);
-        });
+        if (this.state.username === "" || this.state.password === "") {
+            alert("Username or Password cannot be empty");
+            return;
+        }
 
-        if (response && response.status === 200) {
+        let response = await this.getLogin(this.state.username, this.state.password);
+
+        if (response.ok) {
             window.location.href = "/Dashboard";
         } else {
-            console.log("Login failed");
+            const errorMessage = await response.text();
+            console.error(errorMessage);
+            alert(errorMessage);
         }
     };
-       
+
 
     handleUsername = (event) => {
         this.setState({ username: event.target.value });
@@ -45,20 +52,43 @@ class LoginPage extends Component {
 
     render() {
         return (
-            <div className="w-screen min-h-screen bg-gray-600 flex justify-center pt-2">
-                <div className="w-3/4 bg-indigo-600 rounded-lg pl-10 pt-10 flex justify-center grid content-center">
-                    <div className=" border-2 p-2 rounded-lg border-blue-500">
-                        <h1 className="text-4xl text-left text-white ">Login</h1>
-                        <label className="text-white" >Username</label><br />
-                        <input className="rounded-md w-72 p-1" type="text" onChange={this.handleUsername} /><br />
-                        <label className="text-white">Password</label><br />
-                        <input className="rounded-md w-72 p-1" type="password" onChange={this.handlePassword} /><br />
-                        <button className="bg-blue-500 rounded-md text-white w-72 mt-3" type="button" onClick={this.handleLogin}>Login</button>
+            <div className="w-screen min-h-screen bg-gray-600 flex justify-center items-center">
+                <div className="w-3/4 bg-indigo-600 rounded-lg p-10">
+                    <h1 className="text-4xl text-center text-white mb-4">Login</h1>
+                    <div className="grid grid-cols-1 gap-y-4">
+                        <div>
+                            <label className="text-white block mb-2" htmlFor="username">Username</label>
+                            <input
+                                id="username"
+                                className="rounded-md w-full p-2"
+                                type="text"
+                                onChange={this.handleUsername}
+                            />
+                        </div>
+                        <div>
+                            <label className="text-white block mb-2" htmlFor="password">Password</label>
+                            <input
+                                id="password"
+                                className="rounded-md w-full p-2"
+                                type="password"
+                                onChange={this.handlePassword}
+                            />
+                        </div>
+                        <div>
+                            <button
+                                className="bg-blue-500 rounded-md text-white w-full p-2"
+                                type="button"
+                                onClick={this.handleLogin}
+                            >
+                                Login
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
+
 }
 
 export default LoginPage;
