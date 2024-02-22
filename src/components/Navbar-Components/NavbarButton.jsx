f7e607a391b17ba4ae3b69d7e2045446b262196b
Pepijn  van den Ende <PS.vandenEnde@student.han.nl>
2024-02-22 22:23:29 +0100

    fogor

--- src/components/Navbar-Components/NavbarButton.jsx
+++ src/components/Navbar-Components/NavbarButton.jsx
@@ -11,9 +11,9 @@ class NavbarButton extends Component {
     }
     render() {
         return (
-            <div className="p-2 mr-2 w-max border-indigo-600 border-2 rounded text-center my-2 bg-indigo-600 text-white">
-                <Link to={`/${this.state.link}`}>
-            </div>
+            <Link to={`/${this.state.link}`} className="p-2 mr-2 w-max border-indigo-600 border-2 rounded text-center my-2 bg-indigo-600 text-white cursor-pointer">
+                {this.state.name}
+            </Link >
         );
     }
 
