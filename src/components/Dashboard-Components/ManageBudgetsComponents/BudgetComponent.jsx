e3b74ce86eda813e77d542b72147945662ff6d32
Pepijn  van den Ende <PS.vandenEnde@student.han.nl>
2024-04-12 22:43:23 +0200

    last update b4 move

--- src/components/Dashboard-Components/ManageBudgetsComponents/BudgetComponent.jsx
+++ src/components/Dashboard-Components/ManageBudgetsComponents/BudgetComponent.jsx
@@ -1,10 +1,10 @@
 const BudgetComponent = ({ budget }) => {
     return (
         <div className="border-2 border-gray-300 rounded-lg p-2 m-2">
-            <div className="border-b-2 border-gray-300 text-3xl text-center">{budget.name}</div>
+            <div className="border-b-2 border-gray-300 text-3xl text-center mb-1 pb-2">{budget.name}</div>
             <div className="flex justify-around">
                 <label className="text-xl">Set budget:</label>
-                <input type="text" pattern="\d+,\d{2}" placeholder={"€" + budget.amount} className="w-1/2 h-10 border-2 border-gray-300 p-2 truncate rounded-lg" />
+                <input type="text" pattern="\d+,\d{2}" placeholder={"€" + budget.amount} className="w-min mx-2 h-10 border-2 border-gray-300 p-2 truncate rounded-lg" />
                 <button className="w-min h-min border-2 border-gray-300 button-3d">Save</button>
             </div>
         </div>
